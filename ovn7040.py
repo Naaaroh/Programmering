e4f77f4d258ecc6081da9e8ce7238c7e87af7a84
Lukas Andersson <lukas.andersson05@skola.stenungsund.se>
2023-09-14 13:29:32 +0200

    wfa

---
+++ ovn7040.py
@@ -0,0 +1,9 @@
+#namn = input("Vilken är din favoritfärg")
+#print("Din favorit färg är", namn)
+
+namn = input("Vad heter du?")
+print(namn, "är ett fint namn")
+mat = input("Vad gillar du för mat?")
+print(mat, "är väldigt gott")
+pengar = input("Hur mycket pengar har du?")
+print(namn, "har du verkligen", pengar, "kronor")
