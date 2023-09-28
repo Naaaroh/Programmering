8fa15e5339b3918f93a9e13d39a96d9ba71f5825
Lukas Andersson <lukas.andersson05@skola.stenungsund.se>
2023-09-28 10:37:34 +0200

    while

---
+++ Uppgifter/Test övningar/While_slingor_test/skriv_while-sats_1b.py
@@ -0,0 +1,4 @@
+tal = 25
+while tal > 9:
+    print(tal)
+    tal = tal - 5
---
+++ Uppgifter/Test övningar/While_slingor_test/skriv_while-sats_1c.py
@@ -0,0 +1,7 @@
+svar = "ja"
+
+while svar == "ja":
+    print("Du är bäst! ")
+    svar = input("Vill du höra igen? ")
+else:
+    print("Tack och hej! ")
