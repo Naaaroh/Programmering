c38b3696262724dd8364210b051465876d299fa5
Lukas Andersson <lukas.andersson05@skola.stenungsund.se>
2023-11-30 11:52:27 +0100

    testprov10

---
+++ Uppgifter/Test_prov/prov10/prov10_1.py
@@ -0,0 +1,46 @@
+def maximum(x, y):
+    if x > y:
+        return x
+    else:
+        return y 
+
+print(maximum(3, 7))
+
+print(maximum(11, 3))
+
+
+def addera(x, y):
+    return x + y
+
+print(addera(3, 5))
+
+print(addera(10, 2))
+
+
+def hej(x):
+    return "Hej " + x
+
+print(hej("Ada"))
+
+print(hej("Babbage"))
+
+
+
+
+
+
+
+#print(maximum(3, 7))
+# Funktionen maximum returnerar det största talet.
+# utskrift: 7
+#print(maximum(11, 3))
+# utskrift: 11
+#print(addera(3, 5))
+# Funktionen addera lägger ihop talen och returnerar svaret.
+# utskrift 8
+#print(addera(10, 2))
+# utskrift 12
+#hej("Ada")
+# utskrift: hej Ada
+#hej("Babbage")
+# utskrift: hej Babbage 
