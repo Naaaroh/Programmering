59cdec627d5dd9516d08386d04034334dce9e1c0
Lukas Andersson <lukascolinvalentin@gmail.com>
2024-04-18 13:57:06 +0200

    användar val

--- projekt/projekt.py
+++ projekt/projekt.py
@@ -1,10 +1,28 @@
 import random
 
 alternativ = ["sten", "sax", "påse"]
+
+dator_val = random.choice(alternativ)
+
 spela = input("Vill du spela sten sax påse? Ja eller nej ")
 
 if spela.lower() == ("ja"):
-    val = input("Välj sten, sax eller påse ")
+
+    ditt_val = input("Välj sten, sax eller påse ")
+
+    print("Du valde",ditt_val)
+    print("Jag valde",dator_val)
+
+    if dator_val == ditt_val: 
+        print("Det blev lika")
+
+    elif
+
+    elif
+
+    elif
+
+    else:
 
 else:
     print("Nej, okej")
