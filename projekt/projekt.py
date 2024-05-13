9416b3b9fcd0e0ef45e0bbfc3b0e804df5e8c412
Lukas Andersson <lukascolinvalentin@gmail.com>
2024-05-13 12:29:45 +0200

    spela igen

--- projekt/projekt.py
+++ projekt/projekt.py
@@ -4,29 +4,39 @@ alternativ = ["sten", "sax", "påse"]
 
 dator_val = random.choice(alternativ)
 
-spela = input("Vill du spela sten sax påse? Ja eller nej ")
+def a():
+    if spela.lower() == ("ja"):
 
-if spela.lower() == ("ja"):
+        ditt_val = input("Välj sten, sax eller påse ")
 
-    ditt_val = input("Välj sten, sax eller påse ")
+        print("Du valde",ditt_val)
+        print("Jag valde",dator_val)
 
-    print("Du valde",ditt_val)
-    print("Jag valde",dator_val)
+        if dator_val == ditt_val: 
+            print("Det blev lika")
 
-    if dator_val == ditt_val: 
-        print("Det blev lika")
+        elif ditt_val == "påse" and dator_val == "sten":
+            print("Du vann")
 
-    elif ditt_val == "påse" and dator_val == "sten":
-        print("Du vann")
+        elif ditt_val == "sax" and dator_val == "påse":
+            print("Du vann")
 
-    elif ditt_val == "sax" and dator_val == "påse":
-        print("Du vann")
+        elif ditt_val == "sten" and dator_val == "sax":
+            print("Du vann")
 
-    elif ditt_val == "sten" and dator_val == "sax":
-        print("Du vann")
+        else:
+            print("Jag vann")
 
     else:
-        print("Jag vann")
+        print("Nej, okej")
 
-else:
-    print("Nej, okej")
+spela = input("Vill du spela sten sax påse? Ja eller nej ")
+
+a()
+
+spela_igen = input("Vill du spela igen? ")
+
+while spela_igen.lower() == ("ja"):
+    a()
+else: 
+    print("Nej okej")
