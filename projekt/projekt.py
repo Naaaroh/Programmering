f962917a8b4c1bce5e85ea18b3f55cdc0f491ea5
Lukas Andersson <lukascolinvalentin@gmail.com>
2024-05-23 13:36:08 +0200

    bug_fixar

--- projekt/projekt.py
+++ projekt/projekt.py
@@ -1,27 +1,26 @@
 import random
 
 alternativ = ["sten", "sax", "påse"]
-
 dator_val = random.choice(alternativ)
 
 def a():
-    if spela.lower() == ("ja"):
+    if spela.lower() == ("ja") or spela.lower() == ("yes"):
 
         ditt_val = input("Välj sten, sax eller påse ")
 
         print("Du valde",ditt_val)
         print("Jag valde",dator_val)
 
-        if dator_val == ditt_val: 
+        if dator_val == ditt_val.lower(): 
             print("Det blev lika")
 
-        elif ditt_val == "påse" and dator_val == "sten":
+        elif ditt_val.lower() == "påse" and dator_val == "sten":
             print("Du vann")
 
-        elif ditt_val == "sax" and dator_val == "påse":
+        elif ditt_val.lower() == "sax" and dator_val == "påse":
             print("Du vann")
 
-        elif ditt_val == "sten" and dator_val == "sax":
+        elif ditt_val.lower() == "sten" and dator_val == "sax":
             print("Du vann")
 
         else:
@@ -31,12 +30,14 @@ def a():
         print("Nej, okej")
 
 spela = input("Vill du spela sten sax påse? Ja eller nej ")
-
+    
 a()
 
 spela_igen = input("Vill du spela igen? ")
 
 while spela_igen.lower() == ("ja"):
+    dator_val = random.choice(alternativ)
+
     a()
     spela_igen = input("Vill du spela igen? ")
 else: 
